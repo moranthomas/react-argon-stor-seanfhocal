30447534fb8348b01a2dc00bc6158715b4a576fb
moranthomas <thomas@optimizme.com>
2020-10-03 17:07:52 +0100

    initial commit

---
+++ src/views/Index.js
@@ -0,0 +1,61 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Container, Row } from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.js";
+import CardsFooter from "components/Footers/CardsFooter.js";
+
+// index page sections
+import Hero from "./IndexSections/Hero.js";
+import Navbars from "./IndexSections/Navbars.js";
+import Tabs from "./IndexSections/Tabs.js";
+
+class Index extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main ref="main">
+          <Hero />
+          <section className="section">
+            <Container>
+            </Container>
+          </section>
+          <Navbars />
+          <section className="section section-components">
+            <Container>
+              <Tabs />
+            </Container>
+          </section>
+        </main>
+        <CardsFooter />
+      </>
+    );
+  }
+}
+
+export default Index;
