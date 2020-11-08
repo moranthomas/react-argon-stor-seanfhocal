35c078cc1eb1c6a002dedb2be8c9deb257dc8bf3
moranthomas <thomas@optimizme.com>
2020-11-08 21:47:11 +0000

    Adding author page

---
+++ src/views/Author.js
@@ -0,0 +1,29 @@
+/* IRISH
+
+Ta Gort na Gaeilge a threabhadh ag Tomas le fada and la. Bhi se ina phriomhoide bunscoile ar feadh cuig bliana is daichead. Ta se de thaithi aige daltai bunscoile, daltai meanscoile, abhair oidi agus mic leinn ollscoile a bheith muinte aige.
+
+Ta ceim B.A, Ard Teastas Gaeilge, M.Ed, agus Ard Teastas san Oideachas (Cead Onoracha) aige.
+
+Deanann se obair anois do Cholaiste Mhuire Marino agus do Cholaiste Hibernia. Thug se leachtai don Dioploma sa Ghaeilge in Ollscoil na Gaillimhe ar feadh se bliana deag.
+
+Ta treimshi eagsula caite ag Tomas mar Phriomhoide i gColaisti ins na Gaeltachtai difriula ar feadh na mblianta, an cean is deanai na Colaiste Gharumna, Tir and Fhia ar feadh deich mbliana
+
+Ar ndoigh, tuigeann se tabhacht agus fiuntas na seanfhocal maraon leis an ghaois ata iontu. Is minic seanfhocail mar theidil aisti is minic iad mar ornaidiocht i lar aisti.
+
+- Nollaig Heuston, Iar-Roinnchigire na Roinne Oideachais agus Scileanna
+
+/*
+
+
+/* ENGLISH
+
+Tomas has been plowing Gort na Gaeilge for a long time. He was a primary school principal for forty-five years. He has experience of teaching primary school students, secondary school students, student teachers and university students.
+
+He holds a B.A, Higher Certificate in Irish, M.Ed, and Higher Certificate in Education (Honors Permission).
+
+He now works for Our Lady of Marino College and Hibernia College. He lectured for the Diploma in Irish at the University of Galway for sixteen years.
+
+Tomas has served as a Principal in Colleges in the various Gaeltachts for many years, most recently at Coláiste Gharumna, Tir and Fhia for ten years.
+
+- Nollaig Heuston,- Former Inspector of the Department of Education and Skills
+*/
